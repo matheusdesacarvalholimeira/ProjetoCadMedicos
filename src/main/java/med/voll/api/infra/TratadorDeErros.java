c380d129b109f28454d76a6965aceb7d2b46729a
Matheus <118131034+matheusdesacarvalholimeira@users.noreply.github.com>
2024-02-16 01:12:11 -0300

    first commit

---
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -0,0 +1,37 @@
+package med.voll.api.infra;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErrro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+
+        List<DadosErroValidacao> erros = ex.getFieldErrors().stream()
+                .map(DadosErroValidacao::new)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(erros);
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
